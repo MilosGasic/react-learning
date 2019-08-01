576573f46351c18c285672248cb5423603b2f2b3
MilosGasic <milos.gasic1986@gmail.com>
2019-08-01 19:16:15 +0200

    prvi komit

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import Person from './Person/Person';
+
+class Persons extends Component {
+    // static getDerivedStateFromProps (props, state){
+    //    console.log ('[Persons.js] getDerivedStateFromProps');     
+    // return state;        
+    // }
+
+
+     shoudComponentUpdate (nextProps, nextState) {
+             console.log ('[Persons.js] shoudComponentUpdate');
+             if (nextProps.persons !== this.props.persons) {
+                     return true;
+             } else {
+                     return false;
+             }
+     }
+
+     getSnapshotBeforeUpdate (prevProps, prevState) {
+             console.log ('[Persons.js] getSnapshotBeforeUpdate');
+             return {message: 'Snapshot!'};
+     }
+     componentDidUpdate (prevProps, prevState, snapshot) {
+             console.log ('[Person.js] componentDidUpdate');
+             console.log (snapshot);
+     }   
+
+     componentWillUnmount () {
+             console.log('[Persons.js] componentWillUnmount');
+     }
+
+render () {
+console.log ('[Persons.js] rendering...');
+return this.props.persons.map ( (person, index) => {
+        return( 
+        <Person
+        click= {() => this.props.clicked(index)}
+         name={person.name} 
+         age={person.age}
+         key={person.id}
+         changed = {event => this.props.changed (event, person.id)} 
+         />
+        );
+        });
+}    
+
+};
+
+        export default Persons;
---
+++ src/containers/App.js
@@ -0,0 +1,154 @@
+import React, {Component} from 'react';
+// import './App.css';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    console.log ('[App.js] constructor');
+    
+  }
+  state = {
+    persons: [
+      { id: 'asfa1', name: 'Max', age:28},
+      { id: 'vasdf1', name: 'Manu', age:29},
+      { id: 'asdf11', name: 'Stephanie', age:26}
+    ],
+    otherState: 'some other value',
+    showPersons: false,
+    showCockpit: true
+  }
+  
+  static getDerivedStateFromProps (props, state) {
+    console.log ('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  // componentWillMount () {
+  //  console.log ('[App.js] componentDidMount');
+  // }
+
+  componentDidMount () {
+    console.log ('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log ('[App.js] shoudComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate () {
+    console.log ('[App.js] componentDidUpdate');
+  }
+
+nameChangedHandler = (event, id) => {
+const personIndex= this.state.persons.findIndex(p => {
+  return p.id === id;
+});
+
+const person={
+...this.state.persons[personIndex]};
+
+person.name = event.target.value;
+
+const persons =[...this.state.persons];
+persons[personIndex]=person;
+
+    this.setState( {persons: persons} );
+  }
+
+  deletePersonHandler = (personIndex) => {
+// const persons = this.state.persons.slice ();
+const persons = [...this.state.persons];
+persons.splice (personIndex, 1);
+
+this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+
+  }
+
+  render () {
+    console.log('[App.js] render');
+    let persons = null ;
+
+
+if (this.state.showPersons) {
+persons = <Persons 
+    persons={this.state.persons}
+    clicked={this.deletePersonHandler}
+    changed={this.nameChangedHandler} />;
+
+}
+
+     return (
+   
+
+      
+
+      <div>
+        <div style={{
+          color:'red'
+        }}> NEKITAMOTEKS</div>
+
+
+
+<div style={{
+          color:'red',
+          fontSize:25
+        }}>DRUGI TREXT</div>
+
+
+
+        <button onClick={() => {this.setState({showCockpit:false});
+      }}
+      >
+        Remove Cockpit
+        </button>
+       {this.state.showCockpit ? <Cockpit 
+        title={this.props.appTitle}
+        showPersons={this.state.showPersons}
+        personsLength={this.state.persons.length}
+        clicked = {this.togglePersonsHandler}/>
+        : null}
+        {persons}
+      </div>
+    
+     );
+
+  }
+}
+
+export default App;
+
+// <Person 
+// name= {this.state.persons[0].name} 
+// age={this.state.persons[0].age}/>
+// <Person 
+// name= {this.state.persons[1].name} 
+// age={this.state.persons[1].age}
+// click = {this.switchNameHandler.bind(this, 'Maxi!')}
+// changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+// <Person 
+// name={this.state.persons[2].name} 
+// age={this.state.persons[2].age}/> 
+
+
+// switchNameHandler = (newName) => {
+    // console.log ('Was clicked!');
+
+    // DONT DO THIS:this.state.persons [0].name = 'Maximilian';
+
+   // this.setState( {
+     // persons: [
+      // { name: newName, age:28},
+      // { name: 'Manu', age:29},
+      // { name: 'Stephanie', age:27}
+    // ] })
+  // }
